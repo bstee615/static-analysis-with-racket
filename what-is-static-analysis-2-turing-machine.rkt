ad9f9f10ae4e85933156a0d831fe321f28d49597
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-01-29 15:53:41 -0600

    Add abstract interpretation for turing machine

--- what-is-static-analysis-2-turing-machine.rkt
+++ what-is-static-analysis-2-turing-machine.rkt
@@ -13,6 +13,7 @@
 ;        |  <int>
 ;        |  <var>
 
+; add a while construct to Racket
 (define-syntax while 
   (syntax-rules ()
     [(_ cond body ...)
@@ -26,7 +27,7 @@
 (struct state {stmts env})
 
 ; stmt-map : label => stmt*
-(define stmt-map (make-hash))
+(define stmt-map (make-hasheq))
 
 ; constructs the label-to-statements map:
 (define (preprocess stmts)
@@ -41,9 +42,12 @@
     ['()
      (void)]))
 
+; an environment maps variables to integers:
+(define env0 (make-immutable-hasheq))
+
 ; inject : prog -> state
 (define (inject prog)
-  (state prog #hasheq()))
+  (state prog env0))
 
 (define (run prog)
   (preprocess prog)
@@ -87,7 +91,6 @@
 (define (simple-eval exp env)
   (match exp
     [(? symbol?)
-     ;=>
      (hash-ref env exp)]
     [(? integer?)
      exp]
@@ -109,8 +112,227 @@
 )
 
 
-;;;;; Program
 
+
+;;;;;;; abstract operators
+; alpha : int -> abs-int
+; turns an integer into its abstract integer representation.
+; abs-int is a set of signs that can represent an int.
+(define (alpha int)
+  (cond
+    [(< int 0) {set `-}]
+    [(= int 0) {set `0}]
+    [(> int 0) {set `+}]))
+
+; test
+;(alpha 10)
+;(alpha 0)
+;(alpha -200)
+
+
+; simple-eval^ : expr -> abs-int
+; abstract sign analyzer for our language.
+(define (simple-eval^ exp env^)
+  (match exp
+    [(? integer?)
+     (alpha exp)]
+    [(? symbol?)
+     (hash-ref env^ exp)]
+    [`(*, exp1, exp2)
+      (*^ (simple-eval^ exp1 env^)
+         (simple-eval^ exp2 env^))]
+    [`(+, exp1, exp2)
+      (+^ (simple-eval^ exp1 env^)
+         (simple-eval^ exp2 env^))]
+    [`(=, exp1, exp2)
+      (=^ (simple-eval^ exp1 env^)
+          (simple-eval^ exp2 env^))]
+    [`(/, exp1, exp2)
+      (/^ (simple-eval^ exp1 env^)
+          (simple-eval^ exp2 env^))]
+  )
+)
+
+; test
+;(simple-eval^ (+ 0 1) '())
+
+; set-equal operator
+(define (set-equal? A B)
+  (and (subset? A B)
+       (subset? B A)))
+
+; =^: abs-int abs-int -> {0 1}
+(define (=^ a b)
+  (if
+   (set-empty? (set-intersect a b))
+   {set '0}
+   (if
+    (and (set-equal? a (set 0)) (set-equal? b (set 0)))
+    {set '+}
+    {set '0 '+})))
+
+; more precise version
+; *^ : abs-int * abs-int -> abs-int
+(define (*^ a b)
+  ; pairwise
+  (define (*^/sign a b)
+    (match* {a b}
+      [{'+ '+} {set '+}]
+      [{'+ '0} {set '0}]
+      [{'+ '-} {set '-}]
+      [{'0 _}  {set '0}]
+      [{'- '+} {set '-}]
+      [{'- '0} {set '0}]
+      [{'- '-} {set '+}]))
+  ; setwise
+  (apply set-union
+         (for*/list ([s1 a]
+                     [s2 b])
+           (*^/sign s1 s2))))
+
+; +^ : abs-int + abs-int -> abs-int
+(define (+^ a b)
+  (define (+^/sign a b)
+    (match* {a b}
+      [{'+ '+} {set '+}]
+      [{'+ '0} {set '+}]
+      [{'+ '-} {set '+ 0 '-}]
+      [{'0 '+} {set '+}]
+      [{'0 '0} {set '0}]
+      [{'0 '-} {set '-}]
+      [{'- '+} {set '+ 0 '-}]
+      [{'- '0} {set '-}]
+      [{'- '-} {set '-}]))
+  (apply set-union
+         (for*/list ([s1 a]
+                     [s2 b])
+           (+^/sign s1 s2))))
+
+; /^ : abs-int * abs-int -> abs-int
+(define (/^ a b)
+  (define (/^/sign a b)
+    (match* {a b}
+      [{'+ '+} {set '+}]
+      [{'+ '0} {set '0}]
+      [{'+ '-} {set '-}]
+      [{'0 _}  {set '0}]
+      [{'- '+} {set '-}]
+      [{'- '0} {set '0}]
+      [{'- '-} {set '+}]))
+  (apply set-union
+         (for*/list ([s1 a]
+                     [s2 b])
+           (/^/sign s1 s2))))
+
+; test
+;(*^ {set '- '+} {set '+})
+;(+^ {set '+} {set '+})
+;(+^ {set '+} {set '+ '-})
+;(/^ {set '- '+} {set '+})
+
+; states pair statements with environments:
+
+; an environment maps variables to integers:
+(define env^0 (make-immutable-hasheq))
+
+; inject creates an initial state for a program:
+(define (inject^ prog)
+  (state^ prog env^0))
+
+; abstract state
+(struct state^ {stmts env^}
+  #:transparent)
+
+; step^ : state^ -> state^*
+(define (step^ state^0)
+  
+  (define stmts (state^-stmts state^0))
+  
+  (define env^ (state^-env^ state^0))
+  
+  (match stmts
+    ['()
+     env^]
+    
+    [(cons `(label ,l) rest)
+     (list (state^ rest env^))]
+    
+    [(cons `(:= ,var ,exp) rest)
+     (define env^* (hash-set env^ var (simple-eval^ exp env^)))
+     (list (state^ rest env^*))]
+    
+    [(cons `(if ,exp goto ,label) rest)
+     (define condition (simple-eval^ exp env^))
+     (list
+      (state^ (hash-ref stmt-map label) env^)
+      (state^ rest env^))]
+    
+    [(cons `(goto ,label) rest)
+     (list 
+      (state^ (hash-ref stmt-map label) env^))]
+         
+    [else
+     (error (format "unknown instruction: ~a!" (car stmts)))]))
+
+(define (analyze prog)
+  
+  (preprocess prog)
+  
+  ; the initial abstract state:
+  (define state^0 (inject^ prog))
+  
+  ; the set of all states ever seen:
+  ; visited : (set state^)
+  (define visited (set))
+  
+  ; the neighbor maps
+  ; neighbors : state^ #> (list state^)
+  (define neighbors (make-hasheq))
+  
+  ; mark the neigbors of a state.
+  (define (mark-neighbors! state^ succs)
+    (hash-set! neighbors state^ succs))
+  
+  ; marks a state as seen:
+  (define (mark-seen! state^)
+    (set! visited (set-add visited state^)))
+  
+  ; checks if a state is seen
+  (define (seen? state^)
+    (set-member? visited state^))
+  
+  ; states to explore next:
+  (define todo (list state^0))
+  
+  ; adds states to be explored:
+  (define (push-todos states^)
+    (set! todo (append states^ todo)))
+  
+  ; grabs the next state to be explored:
+  (define (pop-todo)
+    (define next (car todo))
+    (set! todo (cdr todo))
+    next)
+  
+  (while (not (null? todo))
+    (define curr (pop-todo))
+    (when (not (seen? curr))
+      (mark-seen! curr)
+      (define succs (step^ curr))
+      (when (list? succs)
+        (mark-neighbors! curr succs)
+        (push-todos succs))))
+  
+  ; return all visited states:
+  neighbors)
+
+
+
+
+
+
+
+;;;;;;;; program
 ; Example program
 (define prog 
   '(
@@ -133,13 +355,17 @@
     
     ))
 (run prog)
+(analyze prog)
 
 ; Loop from 0 to 10
-(run '(
+(define prog2
+  '(
        (:= n 0)
        (label loop)
        (if (= n 10) goto done)
        (:= n (+ n 1))
        (goto loop)
        (label done)
-       ))
+   ))
+(run prog2)
+(analyze prog2)
